7d76cdafc21a33e0ce5300c916f7940fcd15ab20
Pavan Kumar Patruni <>
2019-09-06 18:21:53 +0530

    Updates

---
+++ precache-manifest.e1a58339a8290698cbf7fed5f68a14d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4076b3440f56fdbf70c3bb8ffc4ef23",
+    "url": "/Match-Pair-Memory-Game/index.html"
+  },
+  {
+    "revision": "cc6207c38ff2566ca606",
+    "url": "/Match-Pair-Memory-Game/static/css/main.0f1cb0c1.chunk.css"
+  },
+  {
+    "revision": "a5e203a85212c027c227",
+    "url": "/Match-Pair-Memory-Game/static/js/2.987e4499.chunk.js"
+  },
+  {
+    "revision": "cc6207c38ff2566ca606",
+    "url": "/Match-Pair-Memory-Game/static/js/main.9d785e9e.chunk.js"
+  },
+  {
+    "revision": "6127c3849502675b88df",
+    "url": "/Match-Pair-Memory-Game/static/js/runtime~main.e1f29a30.js"
+  }
+]);
